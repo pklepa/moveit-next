f963a51dd2515e567eaa65d744a2b2b3f8a737ad
Pedro Klepa <pedro.klepa@gmail.com>
2021-02-23 13:31:52 +0000

    Add basic countdown functionality to Countdown component

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,28 +1,53 @@
-import { ReactElement } from 'react';
+import { ReactElement, useEffect, useState } from 'react';
 
 import styles from '../styles/components/Countdown.module.scss';
 
 interface Props {}
 
 function Countdown(): ReactElement<Props> {
+  const [time, setTime] = useState(25 * 60);
+  const [isTimeRunning, setIsTimeRunning] = useState(false);
+
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
+
+  const [minuteLeft, minuteRight] = String(minutes).padStart(2, '0').split('');
+  const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('');
+
+  function startCountdown() {
+    setIsTimeRunning(true);
+  }
+
+  useEffect(() => {
+    if (isTimeRunning && time > 0) {
+      setTimeout(() => {
+        setTime(time - 1);
+      }, 1000);
+    }
+  }, [isTimeRunning, time]);
+
   return (
     <div>
       <div className={styles.countdown_container}>
         <div>
-          <span>2</span>
-          <span>5</span>
+          <span>{minuteLeft}</span>
+          <span>{minuteRight}</span>
         </div>
 
         <span>:</span>
 
         <div>
-          <span>0</span>
-          <span>0</span>
+          <span>{secondLeft}</span>
+          <span>{secondRight}</span>
         </div>
       </div>
 
       <div>
-        <button className={styles.countdown_button} type="button">
+        <button
+          onClick={startCountdown}
+          className={styles.countdown_button}
+          type="button"
+        >
           Start a cicle
         </button>
       </div>
